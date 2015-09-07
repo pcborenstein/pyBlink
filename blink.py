6b35302912b7220117da9cea5b27e410ccace70c
Peter <pcborenstein@gmail.com>
2015-09-07 10:17:48 -0400

    added a "number of blinks" option

--- blink.py
+++ blink.py
@@ -6,13 +6,16 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dur", help="set durreation of time the LED is lit", type=float, default=0.5) 
+parser.add_argument("-n", "--number", help="number of blinks", type=int, default=1)
 args = parser.parse_args()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
-GPIO.output(21, True)
 
-time.sleep(args.dur)
+for i in range(args.number):
+    GPIO.output(21, True)
+    time.sleep(args.dur)
+    GPIO.output(21, False)
+    time.sleep(args.dur)
 
-GPIO.output(21, False)
 GPIO.cleanup()
